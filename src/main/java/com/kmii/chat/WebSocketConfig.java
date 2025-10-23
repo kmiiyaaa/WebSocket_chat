cef360b14ba4f13f45cb27891782c8bbb432f39f
kmiiyaaa <ckm1533@naver.com>
2025-10-23 17:35:10 +0900

    chathandler , confighandler 추가

---
+++ src/main/java/com/kmii/chat/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.kmii.chat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	private ChatHandler chatHandler;
+	
+	
+	// WebSocket 핸들러 등록 메서드
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(chatHandler, "/ws/chat")
+		.setAllowedOrigins("*"); //모든 도메인에서 들어오는 요청 허용
+		
+	}
+
+	
+}
